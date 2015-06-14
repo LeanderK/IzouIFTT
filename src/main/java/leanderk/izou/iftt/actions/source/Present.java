288a7fcd034a57d7e22369f400619117a9f25edf
Leander Kurscheidt <Leander.Kurscheidt@gmx.de>
2015-06-14 13:58:12 +0200

    added features

--- src/main/java/leanderk/izou/iftt/actions/source/Present.java
+++ src/main/java/leanderk/izou/iftt/actions/source/Present.java
@@ -8,6 +8,7 @@ import org.intellimate.izou.events.EventModel;
 import org.intellimate.izou.sdk.Context;
 import org.intellimate.izou.sdk.frameworks.presence.events.PresenceEvent;
 
+import java.time.LocalDate;
 import java.util.Collections;
 
 /**
@@ -19,15 +20,19 @@ public class Present extends Action implements SourceAction, EventListenerModel 
     public static final String NON_STRICT = "nonStrict";
     public static final String EVERY_TIME = "everyTime";
     public static final String UNKNOWN = "unknown";
+    public static final String FIRST_DAY = "firstEncounterDay";
+    private LocalDate lastSeen;
     private final boolean nonStrict;
     private final boolean unknown;
     private final boolean everyTime;
+    private final boolean firstEncounterDay;
 
     public Present(String parameter, ActionFlow actionFlow, Context context) throws IllegalArgumentException {
         super(actionFlow, context, ID);
         boolean nonStrict = false;
         boolean unknown = false;
         boolean everyTime = false;
+        boolean firstEncounterDay = false;
         for (String command : parameter.split(",")) {
             command = command.trim();
             switch (command) {
@@ -37,11 +42,14 @@ public class Present extends Action implements SourceAction, EventListenerModel 
                     break;
                 case UNKNOWN: unknown = true;
                     break;
+                case FIRST_DAY: firstEncounterDay = true;
+                    break;
             }
         }
         this.nonStrict = nonStrict;
         this.unknown = unknown;
         this.everyTime = everyTime;
+        this.firstEncounterDay = firstEncounterDay;
         getContext().getEvents().registerEventListener(Collections.singletonList(PresenceEvent.ID), this);
         actionFlow.addToUnregisterCallback(actionFlow1 -> context.getEvents().unregisterEventListener(this));
     }
@@ -53,12 +61,15 @@ public class Present extends Action implements SourceAction, EventListenerModel 
      */
     @Override
     public void eventFired(EventModel event) {
-        if (!nonStrict && !event.containsDescriptor(PresenceEvent.STRICT_DESCRIPTOR))
+        if (firstEncounterDay && !lastSeen.plusDays(1).isEqual(LocalDate.now()))
             return;
-        if (!unknown && !event.containsDescriptor(PresenceEvent.KNOWN_DESCRIPTOR))
+        if (nonStrict && !event.containsDescriptor(PresenceEvent.STRICT_DESCRIPTOR))
             return;
-        if (!everyTime && !event.containsDescriptor(PresenceEvent.FIRST_ENCOUNTER_DESCRIPTOR))
+        if (unknown && !event.containsDescriptor(PresenceEvent.KNOWN_DESCRIPTOR))
             return;
+        if (everyTime && !event.containsDescriptor(PresenceEvent.FIRST_ENCOUNTER_DESCRIPTOR))
+            return;
+        lastSeen = LocalDate.now();
         getActionFlow().start();
     }
 }
